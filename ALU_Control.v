3d5bb7020d0430fcf1ff2ee35481647245f0c51c
shifaaimrann <sm10320@st.habib.edu.pk>
2025-11-23 21:00:14 +0500

    Update ALU_Control.v

--- ALU_Control.v
+++ ALU_Control.v
@@ -18,7 +18,8 @@ always @(*) begin
             4'b0000: begin Operation=4'b0010;end
             4'b1000: begin Operation=4'b0110; end
             4'b0111: begin Operation=4'b0000;end // <-- FIX: Was 4'b0010 (ADD), now 4'b0000 (AND)
-            4'b0110: begin Operation=4'b0001; end            
+            4'b0110: begin Operation=4'b0001; end 
+            4'b0001: begin Operation = 4'b0011;end        
             default: Operation = 4'bxxxx;
         endcase
     end
