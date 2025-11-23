748b73f4c92714f17959c08ab9680eff15baa3fb
shifaaimrann <sm10320@st.habib.edu.pk>
2025-11-23 20:59:46 +0500

    Update muxALU.v

--- muxALU.v
+++ muxALU.v
@@ -15,7 +15,7 @@ if (ALUop==4'b0000)//selector bit is 0000nso AND
     end 
 else if (ALUop==4'b0110)//selector bit is 0110 so we will subtract
     begin 
-    result = a-b;//since its behavioral we will do a-b-carryin
+    result = a-b;//since its behavioral we wont do a-b-carryin
     end
 else if (ALUop==4'b0001)//seelctor bit is 0001, so OR
     begin
@@ -29,8 +29,12 @@ else if (ALUop==4'b1100)//1100 is NOR
     begin
         result = ~(a|b); 
     end 
+else if (ALUop==4'b0011)
+    begin
+        result = a << b;
+    end
 else begin
-    result = 64'dx; // <-- FIX: Default case to prevent latch
+    result = 64'dx; 
 end
  Zero=(result==64'b0);//assigning Zero based on if result is equal to 64 bits of zeroes
 end
